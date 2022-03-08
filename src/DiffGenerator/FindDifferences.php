6cc3e8ff229807a3ddc7a50863963cf0446f0770
Ilya Golovin <foreachq@gmail.com>
2022-03-08 03:50:50 +0300

    refactor: divide methods into smaller ones

--- src/DiffGenerator/FindDifferences.php
+++ src/DiffGenerator/FindDifferences.php
@@ -4,58 +4,102 @@ namespace Differ\DiffGenerator\FindDifferences;
 
 function getDifferences(array $arr1, array $arr2): array
 {
-    $allEntries = array_unique(array_merge(array_keys($arr1), array_keys($arr2)));
+    $allKeys = array_unique(array_merge(array_keys($arr1), array_keys($arr2)));
 
-    $diffBuilder = array_map(function ($key) use ($arr2, $arr1) {
-        if (!in_array($key, array_keys($arr2), true)) {
-            return ['key' => $key, 'state' => 'removed', 'value' => $arr1[$key]];
+    $diffBuilder = [];
+    foreach ($allKeys as $key) {
+        $keyState = getKeyState($arr1, $arr2, $key);
+        if ($keyState !== 'unchanged') {
+            $value = $keyState === 'add' ? $arr2[$key] : $arr1[$key];
+            $diffBuilder[] = generateMeta($keyState, $key, $value);
+            continue;
         }
-        if (!in_array($key, array_keys($arr1), true)) {
-            return ['key' => $key, 'state' => 'add', 'value' => $arr2[$key]];
-        }
-        if ($arr2[$key] !== $arr1[$key]) {
-            if (is_array($arr1[$key]) && is_array($arr2[$key])) {
-                $newValue = getDifferences($arr1[$key], $arr2[$key]);
-                return ['key' => $key, 'state' => 'unchanged', 'value' => $newValue];
-            }
-            return ['key' => $key, 'state' => 'changed', 'oldValue' => $arr1[$key], 'newValue' => $arr2[$key]];
-        }
-        return ['key' => $key, 'state' => 'unchanged', 'value' => $arr1[$key]];
-    }, $allEntries);
 
-    return normalizeDifferences($diffBuilder);
+        if ($arr2[$key] === $arr1[$key]) {
+            $diffBuilder[] = generateMeta('unchanged', $key, $arr1[$key]);
+            continue;
+        }
+
+        if (is_array($arr1[$key]) && is_array($arr2[$key])) {
+            $newValue = getDifferences($arr1[$key], $arr2[$key]);
+            $diffBuilder[] = generateMeta('unchanged', $key, $newValue);
+            continue;
+        }
+        $diffBuilder[] = generateMeta('changed', $key, [$arr1[$key], $arr2[$key]]);
+    }
+
+    return formatEntriesToMeta($diffBuilder);
 }
 
-function normalizeDifferences(array $diffs): array
+function getKeyState(array $keys1, array $keys2, $key): string
+{
+    if (!in_array($key, array_keys($keys2), true)) {
+        return 'removed';
+    }
+    if (!in_array($key, array_keys($keys1), true)) {
+        return 'add';
+    }
+
+    return 'unchanged';
+}
+
+function generateMeta(string $state, $key, $value): array
+{
+    if ($state !== 'changed') {
+        return ['key' => $key, 'state' => $state, 'value' => $value];
+    }
+
+    $oldValue = $value[0];
+    $newValue = $value[1];
+
+    return ['key' => $key, 'state' => $state,
+        'oldValue' => $oldValue, 'newValue' => $newValue];
+}
+
+function formatEntriesToMeta(array $diffs): array
 {
     $keys = array_keys($diffs);
-    return array_map(function ($key) use ($diffs) {
+
+    $result = [];
+    foreach ($keys as $key) {
         $meta = $diffs[$key];
         if (!is_array($meta)) {
-            return ['key' => $key, 'state' => 'unchanged', 'value' => $meta];
+            $result[] = generateMeta('unchanged', $key, $meta);
+            continue;
         }
+
         if (!array_key_exists('state', $meta)) {
-            $newValue = normalizeDifferences($meta);
-            return ['key' => $key, 'state' => 'unchanged', 'value' => $newValue];
-        }
-        $metaKey = $meta['key'];
-        if ($meta['state'] !== 'changed' && is_array($meta['value'])) {
-            $newValue = normalizeDifferences($meta['value']);
-            return ['key' => $metaKey, 'state' => $meta['state'], 'value' => $newValue];
-        }
-        if ($meta['state'] === 'changed') {
-            $oldValue = is_array($meta['oldValue'])
-                ? normalizeDifferences($meta['oldValue'])
-                : $meta['oldValue'];
-
-            $newValue = is_array($meta['newValue'])
-                ? normalizeDifferences($meta['newValue'])
-                : $meta['newValue'];
-
-            return ['key' => $metaKey, 'state' => 'changed',
-                'oldValue' => $oldValue, 'newValue' => $newValue];
+            $newValue = formatEntriesToMeta($meta);
+            $result[] = generateMeta('unchanged', $key, $newValue);
+            continue;
         }
 
-        return ['key' => $metaKey, 'state' => $meta['state'], 'value' => $meta['value']];
-    }, $keys);
+        $result[] = formatMetaValueToMeta($meta);
+    }
+
+    return $result;
+}
+
+function formatMetaValueToMeta(array $meta): array
+{
+    $key = $meta['key'];
+    if ($meta['state'] === 'changed') {
+        $oldValue = is_array($meta['oldValue'])
+            ? formatEntriesToMeta($meta['oldValue'])
+            : $meta['oldValue'];
+
+        $newValue = is_array($meta['newValue'])
+            ? formatEntriesToMeta($meta['newValue'])
+            : $meta['newValue'];
+
+        return generateMeta('changed', $key, [$oldValue, $newValue]);
+    }
+
+    if (is_array($meta['value'])) {
+        $newValue = formatEntriesToMeta($meta['value']);
+
+        return generateMeta($meta['state'], $key, $newValue);
+    }
+
+    return generateMeta($meta['state'], $key, $meta['value']);
 }
